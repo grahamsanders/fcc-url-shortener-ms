9dcb43d1efb711bacf3bc5577ec3d3609dee12d3
Graham Sanders <grahamsanders@gmail.com>
2017-07-30 01:54:25 +0000

    Initial adding of empty express server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.status(200);
+  res.send("");
+});
+
+// Respond not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }  
+})
+
+app.listen(process.env.PORT, function () {
+  console.log('Node.js listening on ' + process.env.PORT + '...');
+});
